93a897f1ea3b83fd1d1b34ed81be61c6d2a32be9
lxhoi <lxhoi.2k@gmail.com>
2025-08-20 13:39:15 +0700

    fix: simplify configuration and downgrade vite for better compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "./",
   build: {
-    assetsInclude: [
-      "**/*.jpeg",
-      "**/*.jpg",
-      "**/*.png",
-      "**/*.svg",
-      "**/*.gif",
-    ],
-
-    copyPublicDir: true,
-
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name].[ext]",
-      },
-    },
+    outDir: "dist",
+    assetsInclude: ["**/*.{jpg,png,svg,gif}"],
+    copyPublicDir: true
   },
 });
