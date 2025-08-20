c5ef74dd7f8e5e81ac42c8ef447751d4704895c7
lxhoi <lxhoi.2k@gmail.com>
2025-08-20 13:06:12 +0700

    Initial commit with pricing page implementation

---
+++ slides.js
@@ -0,0 +1,19 @@
+const slides = [
+  {
+    title: "BASIC",
+    image: "/basic.png",
+    url: "/projects/desert-oasis",
+  },
+  {
+    title: "ELITE",
+    image: "/elite.png",
+    url: "/projects/domed-sanctuary",
+  },
+  {
+    title: "PREMIUM",
+    image: "/premium.png",
+    url: "/projects/courtyard-retreat",
+  },
+];
+
+export default slides;
